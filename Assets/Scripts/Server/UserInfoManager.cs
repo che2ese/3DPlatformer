1d76a25c504a254b25e1b996f826c4038fbefc73
MS_1129 <141838530+che2ese@users.noreply.github.com>
2025-01-31 21:13:14 +0900

    Add Player Money

--- Assets/Scripts/Server/UserInfoManager.cs
+++ Assets/Scripts/Server/UserInfoManager.cs
@@ -8,6 +8,7 @@ public class UserInfoManager : MonoBehaviour
     public string nickname { get; private set; }
     public int character { get; private set; }
     public string role { get; private set; }
+    public int money { get; private set; }
 
     private void Awake()
     {
@@ -22,11 +23,12 @@ public class UserInfoManager : MonoBehaviour
         }
     }
 
-    public void SetUserInfo(string nickname, int character, string role)
+    public void SetUserInfo(string nickname, int character, string role, int money)
     {
-        Debug.Log($"SetUserInfo 호출됨: 닉네임={nickname}, 캐릭터={character}, 역할={role}");
+        Debug.Log($"SetUserInfo 호출됨: 닉네임={nickname}, 캐릭터={character}, 역할={role}, 재화={money}");
         this.nickname = nickname;
         this.character = character;
         this.role = role;
+        this.money = money;
     }
 }
