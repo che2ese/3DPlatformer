bdc7454bb822ba54dc7f7cabcf597aa3c92bcb49
MS_1129 <141838530+che2ese@users.noreply.github.com>
2025-02-12 23:59:36 +0900

    Map Layout and Create Coin

---
+++ Assets/Scripts/Item/Coin.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public Vector3 direction;
+
+    [SerializeField]
+    private GameObject pickUpEffect;
+    [SerializeField]
+    private bool isRed;
+
+    void Update()
+    {
+        transform.Rotate(direction * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Instantiate(pickUpEffect, transform.position, transform.rotation);
+            Destroy(gameObject);
+            if (isRed)
+            {
+                // 맵 코인 증가
+            }
+            else
+            {
+                // 상점 코인 증가
+            }
+        }
+    }
+}
--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -10,6 +10,21 @@ public class Item : MonoBehaviour
 
     private bool destroy;
 
+    private Renderer childRenderer;
+
+    private void Start()
+    {
+        // 자식 오브젝트에서 Renderer 컴포넌트를 찾기
+        childRenderer = GetComponentInChildren<Renderer>();
+
+        // 자식 Renderer가 존재하면 그림자 설정
+        if (childRenderer != null)
+        {
+            childRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On; // 그림자 생성
+            childRenderer.receiveShadows = true;  // 그림자 받기
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         // 플레이어 태그와 충돌 시
@@ -28,7 +43,17 @@ public class Item : MonoBehaviour
 
         if (breakEffect)
         {
-            Instantiate(breakEffect, transform.position, transform.rotation);
+            // breakEffect를 현재 물체와 같은 부모 아래에 생성
+            GameObject effect = Instantiate(breakEffect, transform.position, transform.rotation);
+            effect.transform.SetParent(transform.parent);  // 부모를 현재 오브젝트의 부모로 설정
+
+            // 새로 생성된 breakEffect에도 그림자 설정
+            Renderer effectRenderer = effect.GetComponentInChildren<Renderer>();  // 자식 Renderer 찾기
+            if (effectRenderer != null)
+            {
+                effectRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On; // 그림자 생성
+                effectRenderer.receiveShadows = true;  // 그림자 받기
+            }
         }
 
         if (destroy)
